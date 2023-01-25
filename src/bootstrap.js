9e87018a76def41e0849e69553da708c0b6c197f
Akshaya <rakshayaee@gmail.com>
2023-01-25 15:55:36 +0530

    Changes in rendering mfe1 and mfe2

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,6 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// const ReactMFE1 = import('mfe1/Mfe1Index')
+import('mfe1/Mfe1Index')
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// document.querySelector('#root').append(ReactMFE1);
